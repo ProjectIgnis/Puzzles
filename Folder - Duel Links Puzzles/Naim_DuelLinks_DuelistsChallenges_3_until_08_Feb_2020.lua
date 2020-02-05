85fa1a158138c2ef19a65fcdd2b9abf9a0dd3869
NaimSantos <naimsantos45@gmail.com>
2020-02-05 10:48:57 -0300

    Added two new duel puzzles

---
+++ Folder - Duel Links Puzzles/Naim_DuelLinks_DuelistsChallenges_3_until_08_Feb_2020.lua
@@ -0,0 +1,25 @@
+--[[message
+Reproduces a Duel Link's puzzle, including its hints.
+
+	Your Starting LP: 4000
+	Opponent's Starting LP: 2200
+	Complexity: 1/10.
+	Objective: Win this turn.
+	Hint: You're under arrest! Make way for the Superheavy Samurai!
+]]
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI+DUEL_SPEED,3)
+Debug.SetPlayerInfo(0,4000,0,0)
+Debug.SetPlayerInfo(1,2200,0,0)
+
+--Main Deck
+Debug.AddCard(34496660,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(98045062,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--Monster Zones
+Debug.AddCard(7391448,0,0,LOCATION_MZONE,2,1,true)
+--Monster Zones
+Debug.AddCard(97017120,1,1,LOCATION_MZONE,2,1,true)
+Debug.AddCard(34496660,1,1,LOCATION_MZONE,3,4,true)
+Debug.ShowHint("Win this turn!")
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
---
+++ Folder - Duel Links Puzzles/Naim_DuelLinks_DuelistsChallenges_4_until_08_Feb_2020.lua
@@ -0,0 +1,30 @@
+--[[message
+Reproduces a Duel Link's puzzle, including its hints.
+
+	Your Starting LP: 4000
+	Opponent's Starting LP: 2500
+	Complexity: 1.5/10.
+	Objective: Win this turn.
+	Hint: Figuring out how to use magic should be no problem for a magician, right?
+]]
+
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI+DUEL_SPEED,3)
+Debug.SetPlayerInfo(0,4000,0,0)
+Debug.SetPlayerInfo(1,2500,0,0)
+
+--Main Deck
+Debug.AddCard(46986414,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(21082832,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(73616671,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(7084129,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(47963370,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Spell & Trap Zones
+Debug.AddCard(47222536,0,0,LOCATION_SZONE,2,10)
+--Monster Zones
+Debug.AddCard(82593786,1,1,LOCATION_MZONE,2,4,true)
+Debug.AddCard(80280944,1,1,LOCATION_MZONE,3,1,true)
+Debug.ShowHint("Win this turn!")
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
