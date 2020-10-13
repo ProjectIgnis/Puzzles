cbc8433baa6a8eec8f803fed6e99ea508656959c
Eroldin <54679426+Eroldin@users.noreply.github.com>
2020-10-13 09:44:32 +0200

    Fixed a Redundancy in puzzle 04 Eroldin
    Pot of Benevolence was unnecessary. Puzzle has been slightly changed to fix this.

--- Miscellaneous/Eroldin_04_The_Magician_Family.lua
+++ Miscellaneous/Eroldin_04_The_Magician_Family.lua
@@ -11,7 +11,7 @@ Magicians have always been accused of being devil worshippers. Show them, this i
 Debug.SetAIName('Eroldin: "Prove Your Skill!"')
 Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI)
 Debug.SetPlayerInfo(0,200,0,0)
-Debug.SetPlayerInfo(1,16100,0,0)
+Debug.SetPlayerInfo(1,16400,0,0)
 
 --Challenger's Monster Zone
 local m1=Debug.AddCard(46986414,0,0,LOCATION_MZONE,2,POS_FACEUP_ATTACK) --Dark Magician
@@ -41,32 +41,32 @@ local e2=Debug.AddCard(83584898,1,1,LOCATION_SZONE,4,POS_FACEUP) --Darkword Shac
 Debug.AddCard(30603688,0,0,LOCATION_HAND,0,POS_FACEUP) --Apprentice Illusion Magician
 Debug.AddCard(07198399,0,0,LOCATION_HAND,1,POS_FACEDOWN) --Choco Magician Girl
 Debug.AddCard(38033125,0,0,LOCATION_HAND,2,POS_FACEDOWN) --Dark Magician Girl
-Debug.AddCard(30603688,0,0,LOCATION_HAND,3,POS_FACEDOWN) --Apprentice 
+Debug.AddCard(30208479,0,0,LOCATION_HAND,3,POS_FACEDOWN) --Magician of Black Chaos
 Debug.AddCard(78986941,0,0,LOCATION_HAND,4,POS_FACEDOWN) --Order to Charge
 Debug.AddCard(60709218,0,0,LOCATION_HAND,5,POS_FACEDOWN) --Bond Between Teacher and Student
 Debug.AddCard(25774450,0,0,LOCATION_HAND,6,POS_FACEDOWN) --Mystic Box
 
 --Challenger's Deck
 Debug.AddCard(46986414,0,0,LOCATION_DECK,0,POS_FACEDOWN) --Dark Magician
-Debug.AddCard(46986414,0,0,LOCATION_DECK,1,POS_FACEDOWN) --Dark Magician
-Debug.AddCard(51790181,0,0,LOCATION_DECK,2,POS_FACEDOWN) --Pot of Benevolance
-Debug.AddCard(01784686,0,0,LOCATION_DECK,3,POS_FACEDOWN) --The Eye of Timaeus
-Debug.AddCard(42006475,0,0,LOCATION_DECK,4,POS_FACEDOWN) --Palladium Oracle Mana
-Debug.AddCard(30208479,0,0,LOCATION_DECK,5,POS_FACEDOWN) --MoBC
-Debug.AddCard(30208479,0,0,LOCATION_DECK,6,POS_FACEDOWN) --MoBC
-Debug.AddCard(71490127,0,0,LOCATION_DECK,7,POS_DACEDOWN) --Dragon's Mirror
+Debug.AddCard(46986414,0,0,LOCATION_DECK,0,POS_FACEDOWN) --Dark Magician
+Debug.AddCard(01784686,0,0,LOCATION_DECK,0,POS_FACEDOWN) --The Eye of Timaeus
+Debug.AddCard(42006475,0,0,LOCATION_DECK,0,POS_FACEDOWN) --Palladium Oracle Mana
+Debug.AddCard(30208479,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(30208479,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(71490127,0,0,LOCATION_DECK,0,POS_DACEDOWN) --Dragon's Mirror
+Debug.AddCard(30603688,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 
 --Challenger's Extra Deck
 Debug.AddCard(10000030,0,0,LOCATION_EXTRA,0,POS_FACEDOWN) --Magi Magi Magician Gal
-Debug.AddCard(96471335,0,0,LOCATION_EXTRA,1,POS_FACEDOWN) --Ebon Illusion Magician
-Debug.AddCard(43892408,0,0,LOCATION_EXTRA,2,POS_FACEDOWN) --DMGtDK
-Debug.AddCard(75380687,0,0,LOCATION_EXTRA,3,POS_FACEDOWN) --Amulet Dragon
-Debug.AddCard(41721210,0,0,LOCATION_EXTRA,4,POS_FACEDOWN) --DMtDK
+Debug.AddCard(96471335,0,0,LOCATION_EXTRA,0,POS_FACEDOWN) --Ebon Illusion Magician
+Debug.AddCard(43892408,0,0,LOCATION_EXTRA,0,POS_FACEDOWN) --DMGtDK
+Debug.AddCard(75380687,0,0,LOCATION_EXTRA,0,POS_FACEDOWN) --Amulet Dragon
+Debug.AddCard(41721210,0,0,LOCATION_EXTRA,0,POS_FACEDOWN) --DMtDK
 
 --Eroldin's Grave
 Debug.AddCard(15259703,1,1,LOCATION_GRAVE,0,POS_FACEDOWN) --Toon World
-Debug.AddCard(15259703,1,1,LOCATION_GRAVE,1,POS_FACEDOWN) --Toon World
-Debug.AddCard(15259703,1,1,LOCATION_GRAVE,2,POS_FACEDOWN) --Toon World
+Debug.AddCard(15259703,1,1,LOCATION_GRAVE,0,POS_FACEDOWN) --Toon World
+Debug.AddCard(15259703,1,1,LOCATION_GRAVE,0,POS_FACEDOWN) --Toon World
 
 --Equip commands (Debug.PreEquip(e1,m1))
 Debug.PreEquip(e1,m1)
