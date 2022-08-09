35536d7df2132a11b89faf3b76b8358b58a7b89a
Edoardo Lolletti <edoardo762@gmail.com>
2022-08-09 22:39:21 +0200

    Better formatting

--- RUSH DUEL Dawn of the Battle Royale!!/dq_00038.lua
+++ RUSH DUEL Dawn of the Battle Royale!!/dq_00038.lua
@@ -1,12 +1,18 @@
+Debug.SetAIName("Luke")
 Debug.ReloadFieldBegin(DUEL_MODE_RUSH+DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI+DUEL_PSEUDO_SHUFFLE,0)
 
 Debug.SetPlayerInfo(0,4000,0,0)
-
 Debug.SetPlayerInfo(1,4000,0,0)
 
-Debug.AddCard(160002035,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--Main Deck (yours)
 Debug.AddCard(160301010,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+
+--Hand (yours)
+Debug.AddCard(160002035,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+
+--Main Deck (opponent's)
 Debug.AddCard(160001002,1,1,LOCATION_DECK,0,POS_FACEDOWN)
+
 Debug.ReloadFieldEnd()
 Duel.SetLP(0,100)
 Duel.SetLP(1,2300)
